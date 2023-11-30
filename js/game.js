e791164f9e7ab731012dbb4395317ae1151002f9
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-11-30 15:03:18 +0100

    Implement game logic

--- js/game.js
+++ js/game.js
@@ -1,9 +1,40 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     return (
         choices[Math.floor(Math.random() * choices.length)]
     )
 }
 
-// for debugging
-console.log(getComputerChoice())
+
+let playerSelection = function() {
+    return prompt("Choose your weapon(rock, paper, scissors: ").toLowerCase().trim()
+};
+
+function playRound(computerChoice=getComputerChoice(), userChoice=playerSelection()) {
+    console.log(`Computer: ${computerChoice}\nUser: ${userChoice}`)
+    if (computerChoice === userChoice) {
+        console.log("It's a tie!")
+        playRound()
+    } else if (userChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win!")
+        } else {
+            console.log("You lose.")
+        }
+    } else if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win.")
+        } else {
+            console.log("You lose.")
+        }
+    } else if (userChoice === "scissors") {
+        if (computerChoice === "paper") {
+            console.log("You win.")
+        } else {
+            console.log("You lose.")
+        }
+    }
+}
+
+
+playRound()
